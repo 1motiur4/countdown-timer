14b2cb61791316a63c94ba53446aead36ee8fc42
Motiur Muhith <1motiur4@gmail.com>
2023-05-22 12:30:23 -0400

    boop

--- src/countdown-timer.tsx
+++ src/countdown-timer.tsx
@@ -1,3 +1,26 @@
-const CountdownTimer = () => <div>Add your timer here!</div>;
+import { useState } from "react";
+
+const formatDate = (dateObject) => {
+  const minutes = dateObject.getminutes();
+  const seconds = dateObject.getSeconds();
+  const paddedSeconds = seconds.toString().padStart(2, "0");
+  return `${minutes}:${paddedSeconds}`;
+};
+
+const CountdownTimer = () => {
+  const fiveMinutes = new Date(0, 0, 0, 0, 5);
+  const [timeRemaining, setTimeRemaining] = useState(fiveMinutes);
+
+  return (
+    <div>
+      <h1>{formatDate(timeRemaining)}</h1>
+      <div>
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
+};
 
 export default CountdownTimer;
